08917c66aaaf7f5a313e29d369fc1ac0dccf2fc6
alexiscaspell <alexiscaspell@gmail.com>
2018-05-13 11:16:36 -0300

    Fix import AppConfig en admin service

--- Frontend/src/app/shared/services/admin.service.ts
+++ Frontend/src/app/shared/services/admin.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AppConfig } from '../../app.config';
 
 @Injectable()
 export class AdminService {
