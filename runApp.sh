0ae586aa158f6ad05f69978d9aa472f5945631b1
brianwolf <brian.d.lobo@gmail.com>
2018-05-16 19:18:55 -0300

    subo batch de run en linux funcionando

--- runApp.sh
+++ runApp.sh
@@ -1,6 +1,10 @@
 ###########################################
 #VARS
 ###########################################
+
+#name of console istalled in your linux distro
+CONSOLE="konsole -e"
+
 CURRENT_DIR="$PWD"
 PROJECT_DIR=$CURRENT_DIR'/Backend/cryptoCurrency'
 
@@ -10,11 +14,12 @@ PROJECT_DIR=$CURRENT_DIR'/Backend/cryptoCurrency'
 cd "$PROJECT_DIR"
 export MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n
 #screen mvn clean install tomcat7:run && exit
-mvn clean install tomcat7:run
+$CONSOLE "mvn clean install tomcat7:run" &
 
 ###########################################
 #NODEJS
 ###########################################
 cd "$CURRENT_DIR/Frontend"
 #screen npm install && npm start && exit
-npm install && npm start
+npm install
+npm start
