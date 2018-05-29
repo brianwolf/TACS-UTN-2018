1938aee151adbb7a452dd478af1e9b3576f592b2
alexiscaspell <alexiscaspell@gmail.com>
2018-05-28 21:47:56 -0300

    Segunda version de GenericAbstractDaoImpl y primera implementacion de UserDaoImpl
    Probe traerme todos los nicks y funco, habria que probar lo demas

--- Backend/cryptoCurrency/core/src/main/java/ar/utn/tacs/dao/impl/GenericAbstractDaoImpl.java
+++ Backend/cryptoCurrency/core/src/main/java/ar/utn/tacs/dao/impl/GenericAbstractDaoImpl.java
@@ -1,9 +1,13 @@
 package ar.utn.tacs.dao.impl;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import ar.utn.tacs.dao.GenericDao;
@@ -36,9 +40,42 @@ public abstract class GenericAbstractDaoImpl<T> implements GenericDao{
 		return mongoTemplate.findById(id, clazz);
 	}
 	
-	protected List<T> getByProperty(String propertyName,Class<T> clazz) {
+	protected List<T> getAllByProperty(String propertyName,Object propertyValue,Class<T> clazz) {
 		Query q = new Query();
-		q.fields().include(propertyName);
+		q.addCriteria(Criteria.where(propertyName).is(propertyValue));
 		return mongoTemplate.find(q, clazz);
 	}
+	
+	protected T getByProperty(String propertyName,Object propertyValue,Class<T> clazz) {
+		Query q = new Query();
+		q.addCriteria(Criteria.where(propertyName).is(propertyValue));
+		return mongoTemplate.findOne(q, clazz);
+	}
+	
+	protected <A> List<A> getPropertyAll(String propertyName,Class<A> propertyClazz,Class<T> clazz) {
+		Query q = new Query();
+		q.fields().include(propertyName);
+		List<T> list = mongoTemplate.find(q, clazz);
+		return list.stream().map(o->getProperty(propertyName,o,propertyClazz)).collect(Collectors.toList());
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <A> A getProperty(String propertyName,Object o,Class<A> propertyClazz) {
+		
+		Object finalObject = o;
+		
+		for (String actualProperty : propertyName.split(Pattern.quote("."))) {
+			try {
+				Field f = finalObject.getClass().getDeclaredField(actualProperty);
+				f.setAccessible(true);
+				finalObject = f.get(finalObject);
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return (A) finalObject;
+	}
+	
 }
