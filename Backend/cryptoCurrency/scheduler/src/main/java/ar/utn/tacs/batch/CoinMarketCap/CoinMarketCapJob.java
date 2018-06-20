76a66499e7d53aee55771991f16e1e00df79d9af
brianwolf <brian.d.lobo@gmail.com>
2018-06-20 14:41:46 -0300

    agrego batchs a proyecto scheduler

---
+++ Backend/cryptoCurrency/scheduler/src/main/java/ar/utn/tacs/batch/CoinMarketCap/CoinMarketCapJob.java
@@ -0,0 +1,16 @@
+package ar.utn.tacs.batch.CoinMarketCap;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import ar.utn.tacs.service.external.ExternalService;
+import ar.utn.tacs.util.BeanUtil;
+
+public class CoinMarketCapJob extends QuartzJobBean {
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		BeanUtil.getBean(ExternalService.class).updateCoinMarketCap();
+	}
+}
